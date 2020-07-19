e7c6f717b6acc4a39cb64667bf43ff02991cfb76
ROHKW <elcid2314@gmail.com>
2020-07-20 05:09:45 +0900

    프로그래머스 문제

---
+++ level 2/H-Index.py
@@ -0,0 +1,16 @@
+def solution(citations):
+    answer = len(citations)
+    citations.sort()
+
+    while 1: # 루프
+        cnt =0
+        for i in citations:
+            if i >= answer:
+                cnt+=1
+            if answer <= cnt:
+                return answer
+        answer-=1
+
+    return answer
+
+print(solution([3, 5, 5, 5, 5]))
