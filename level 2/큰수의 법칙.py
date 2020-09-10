904b7b54abbac5f59fae92d0e337539133e1520f
ROHKW <elcid2314@gmail.com>
2020-09-10 17:27:23 +0900

    프로그래머스 문제

---
+++ level 2/큰수의 법칙.py
@@ -0,0 +1,14 @@
+n = map(int, input().split())
+m = map(int, input().split()))
+k = map(int, input().split())
+
+
+data  = list(map(int, input().split()))
+data.sort(reverse = True)
+
+first = data[0]
+second = data[1]
+
+result = first((m // (k+1)) + (m % (k+1)) + second(m % k)
+
+print(result)
