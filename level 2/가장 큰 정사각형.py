82901e5be978cc90de8ba0d3bd7ef04d0f0e4e76
ROHKW <elcid2314@gmail.com>
2020-09-10 17:27:06 +0900

    프로그래머스 문제

--- level 2/가장 큰 정사각형.py
+++ level 2/가장 큰 정사각형.py
@@ -20,8 +20,13 @@ def solution(board):
             mins = min(board[i - 1][j - 1], board[i - 1][j], board[i][j - 1])
             if board[i][j] == 0 or mins == 0:
                 continue
-            board[i][j] = max(mins + 1, board[i][j])
-
-    return max(list(chain.from_iterable(board))) ** 2
-
+    #         board[i][j] = max(mins + 1, board[i][j])
+    #
+    # return max(list(chain.from_iterable(board))) ** 2
+            else:
+                if board[i-1][j] > 0 and board[i][j-1] > 0:
+                    board[i][j] = min(board[i-1][j-1], board[i-1][j], board[i][j-1]) + 1
+                if maxSquare < board[i][j]:
+                    maxSquare = board[i][j]
+    return maxSquare ** 2
 print(solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))
---
+++ level 2/숫자카드겜.py
@@ -0,0 +1,10 @@
+n = map(int, input().split())
+m = map(int, input().split())
+
+finalcard = []
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    z = min(data)
+    for j in data:
+        finalcard +=
