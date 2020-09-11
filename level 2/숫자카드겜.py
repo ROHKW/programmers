169c50c8680994c68c3ed163a0b9574255230b59
ROHKW <62591667+ROHKW@users.noreply.github.com>
2020-09-11 15:19:47 +0900

    Update 숫자카드겜.py
    dd

--- level 2/숫자카드겜.py
+++ level 2/숫자카드겜.py
@@ -6,7 +6,7 @@ finalcard = []
 for i in range(1,n+1):
     data = list(map(int, input().split()))
     Min = min(data)
-    FinalCard = finalcard.append(m)
+    FinalCard = finalcard.append(Min)
     Max =  max(FinalCard)
 
 print(Max)
