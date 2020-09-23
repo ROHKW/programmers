e511e82f6b5180a1d0b624f7656fd0cd1f487501
ROHKW <elcid2314@gmail.com>
2020-09-23 16:48:08 +0900

    프로그래머스 문제

--- 코딩북/시각.py
+++ 코딩북/시각.py
@@ -8,5 +8,5 @@ for i in range(h+ 1):
         for a in range(60):
             if "3" in str(i) + str(j) + str(a):
                 count +=1
-
+# 다 델고와서 문자열로 만들고 그 중에 3이 있냐 없냐로 카운트 해주기?
 print(count)
